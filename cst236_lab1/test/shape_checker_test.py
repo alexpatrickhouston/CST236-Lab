16b903c3f4f3ca7fc3b40d5f1fe05d8d943357a4
alexpatrickhouston <alexpatrickhouston@gmail.com>
2016-01-11 13:18:03 -0800

    Added lab 1 files
    Which only took me like an hour to do cause my computer was freaking right out

---
+++ cst236_lab1/test/shape_checker_test.py
@@ -0,0 +1,15 @@
+"""
+Test for source.shape_checker
+"""
+from source.shape_checker import get_triangle_type
+from unittest import TestCase
+
+class TestGetTriangleType(TestCase):
+
+    def test_get_triangle_equilateral_all_int(self):
+        result = get_triangle_type(1, 1, 1)
+        self.assertEqual(result, 'equilateral')
+
+    def test_get_triangle_scalene_all_int(self):
+        result = get_triangle_type(1, 2, 3)
+        self.assertEqual(result, 'equilateral')
