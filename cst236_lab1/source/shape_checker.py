715803f1fa375b77f31cbb18e8a5811e8248b8db
alexpatrickhouston <alexpatrickhouston@gmail.com>
2016-01-12 01:25:46 -0800

    Lab work complete
    Files were updated to include testing for final 2 triangle tests and
    code/tests were added for 4 sided objects and just rectangle/square
    
    Sorry if the code is a little thin on actually testing I wasn't sure
    what exactly disconnect meant other than more than 360 degrees inside

--- cst236_lab1/source/shape_checker.py
+++ cst236_lab1/source/shape_checker.py
@@ -5,6 +5,7 @@
 The following example code determines if a set of 3 sides of a triangle is equilateral, scalene or iscoceles
 """
 
+
 def get_triangle_type(a=0, b=0, c=0):
     """
     Determine if the given triangle is equilateral, scalene or Isosceles
@@ -48,3 +49,124 @@ def get_triangle_type(a=0, b=0, c=0):
     else:
         return "scalene"
 
+
+def get_rectangle_type(a=0, b=0, c=0, d=0):
+    """
+    Determine if the given rectangle is a rectangle or square
+
+    :param a: line a
+    :type a: float or int or tuple or list or dict
+
+    :param b: line b
+    :type b: float
+
+    :param c: line c
+    :type c: float
+
+    :param d: line d
+    :type d: float
+
+    :return: "square", "rectangle" or "invalid"
+    :rtype: str
+    """
+    if isinstance(a, (tuple, list)) and len(a) == 4:
+        d = a[3]
+        c = a[2]
+        b = a[1]
+        a = a[0]
+
+    if isinstance(a, dict) and len(a.keys()) == 4:
+        values = []
+        for value in a.values():
+            values.append(value)
+        a = values[0]
+        b = values[1]
+        c = values[2]
+        d = values[3]
+
+    if not (isinstance(a, (int, float)) and isinstance(b, (int, float)) and isinstance(c, (int, float)) and isinstance(
+            d, (int, float))):
+        return "invalid"
+
+    if a <= 0 or b <= 0 or c <= 0 or d <= 0:
+        return "invalid"
+
+    if a == b and b == c and c == d:
+        return "square"
+    else:
+        return "rectangle"
+
+
+def get_4sided_type(a=0, b=0, c=0, d=0, ab=0, bc=0, cd=0, da=0):
+    """
+    Determine if the given 4-sided object is a square, rectangle, rhombus or disconnect
+
+    :param a: line a
+    :type a: float or int or tuple or list or dict
+
+    :param b: line b
+    :type b: float
+
+    :param c: line c
+    :type c: float
+
+    :param d: line d
+    :type d: float
+
+    :param d: corner ab
+    :type ab: float
+
+    :param d: corner bc
+    :type bc: float
+
+    :param d: corner cd
+    :type cd: float
+
+    :param d: corner da
+    :type da: float
+
+    :return: "square", "rhombus", "disconnect", "rectangle" or "invalid"
+    :rtype: str
+    """
+    if isinstance(a, (tuple, list)) and len(a) == 8:
+        da = a[7]
+        cd = a[6]
+        bc = a[5]
+        ab = a[4]
+        d = a[3]
+        c = a[2]
+        b = a[1]
+        a = a[0]
+
+    if isinstance(a, dict) and len(a.keys()) == 8:
+        values = []
+        for value in a.values():
+            values.append(value)
+        a = values[0]
+        b = values[1]
+        c = values[2]
+        d = values[3]
+        ab = values[4]
+        bc = values[5]
+        cd = values[6]
+        da = values[7]
+
+    if not (isinstance(a, (int, float)) and isinstance(b, (int, float)) and isinstance(c, (int, float)) and
+            isinstance(d, (int, float)) and isinstance(ab, (int, float)) and isinstance(bc, (
+            int, float)) and isinstance(cd, (int, float)) and isinstance(
+            da, (int, float))):
+        return "invalid"
+
+    if a <= 0 or b <= 0 or c <= 0 or d <= 0 or ab <= 0 or bc <= 0 or cd <= 0 or da <= 0:
+        return "invalid"
+
+    if ab + bc + cd + da > 360:
+        return "disconnect"
+    if a == b and b == c and c == d:
+        if ab == bc == cd == da:
+            return "square"
+        else:
+            return "rhombus"
+
+    else:
+        return "rectangle"
--- cst236_lab1/test/shape_checker_test.py
+++ cst236_lab1/test/shape_checker_test.py
@@ -2,14 +2,60 @@
 Test for source.shape_checker
 """
 from source.shape_checker import get_triangle_type
+from source.shape_checker import get_rectangle_type
+from source.shape_checker import get_4sided_type
 from unittest import TestCase
 
-class TestGetTriangleType(TestCase):
 
+class TestGetTriangleType(TestCase):
     def test_get_triangle_equilateral_all_int(self):
         result = get_triangle_type(1, 1, 1)
         self.assertEqual(result, 'equilateral')
 
     def test_get_triangle_scalene_all_int(self):
         result = get_triangle_type(1, 2, 3)
-        self.assertEqual(result, 'equilateral')
+        self.assertEqual(result, 'scalene')
+
+    def test_get_triangle_invalid_all_int(self):
+        result = get_triangle_type(-1, 1, 1)
+        self.assertEqual(result, 'invalid')
+
+    def test_get_triangle_isosceles_all_int(self):
+        result = get_triangle_type(2, 1, 1)
+        self.assertEqual(result, 'isosceles')
+
+
+class TestGetRectangleType(TestCase):
+    def test_get_rectangle_invalid_all_int(self):
+        result = get_rectangle_type(-1, 1, 1, 1)
+        self.assertEqual(result, 'invalid')
+
+    def test_get_rectangle_square_all_int(self):
+        result = get_rectangle_type(1, 1, 1, 1)
+        self.assertEqual(result, 'square')
+
+    def test_get_rectangle_rectangle_all_int(self):
+        result = get_rectangle_type(1, 2, 2, 1)
+        self.assertEqual(result, 'rectangle')
+
+
+class TestGet4sidedType(TestCase):
+    def test_get_4sided_invalid_all_int(self):
+        result = get_4sided_type(-1, 1, 1, 1, 1, 1, 1, 1)
+        self.assertEqual(result, 'invalid')
+
+    def test_get_4sided_square_all_int(self):
+        result = get_4sided_type(1, 1, 1, 1, 90, 90, 90, 90)
+        self.assertEqual(result, 'square')
+
+    def test_get_4sided_rhombus_all_int(self):
+        result = get_4sided_type(1, 1, 1, 1, 120, 60, 120, 60)
+        self.assertEqual(result, 'rhombus')
+
+    def test_get_4sided_disconnect_all_int(self):
+        result = get_4sided_type(1, 1, 1, 1, 121, 60, 120, 60)
+        self.assertEqual(result, 'disconnect')
+
+    def test_get_4sided_rectangle_all_int(self):
+        result = get_4sided_type(1, 2, 1, 2, 30, 60, 30, 60)
+        self.assertEqual(result, 'rectangle')
